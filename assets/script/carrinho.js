1aab5408d04ec68ff35007bf802bccb8bd7b1ffb
higoramaral <amarahigor@gmail.com>
2025-04-23 14:13:32 -0300

    style: removendo comentarios desnecessarios.

--- assets/script/carrinho.js
+++ assets/script/carrinho.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             carrinho: JSON.parse(localStorage.getItem('carrinho')) || [],
-            desconto: 0 // Armazena o valor do desconto aplicado
+            desconto: 0 
         };
     },
     computed: {
@@ -10,7 +10,7 @@ const app = Vue.createApp({
             return this.carrinho.reduce((total, item) => total + (item.quantidade * item.preco), 0);
         },
         totalComDesconto() {
-            return this.totalCarrinho - this.desconto; // Aplica o desconto ao total
+            return this.totalCarrinho - this.desconto; 
         }
     },
     methods: {
@@ -25,18 +25,18 @@ const app = Vue.createApp({
             if (confirm('Tem certeza de que deseja finalizar a compra?')) {
                 alert('Compra finalizada com sucesso!');
                 this.carrinho = [];
-                this.desconto = 0; // Reseta o desconto após finalizar a compra
+                this.desconto = 0; 
                 this.atualizarCarrinho();
             }
         },
         aplicarDesconto() {
             const cupom = document.getElementById('cupom').value;
-            if (cupom === 'DESCONTO10') {
-                this.desconto = this.totalCarrinho * 0.1; // Aplica 10% de desconto
+            if (cupom === 'Higor10') {
+                this.desconto = this.totalCarrinho * 0.1; 
                 alert('Cupom aplicado com sucesso! Você recebeu 10% de desconto.');
             } else {
                 alert('Cupom inválido.');
-                this.desconto = 0; // Garante que o desconto seja zerado se o cupom for inválido
+                this.desconto = 0; 
             }
         }
     }
